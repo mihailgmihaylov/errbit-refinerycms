419900d5fdc0f527e6d62f70e54262705cd52996
Mihail Mihaylov <mihail.mihaylov@tradeo.com>
2017-05-29 10:07:47 +0300

    Fix code style and indentation

--- manifests/errbit-init.pp
+++ manifests/errbit-init.pp
@@ -1,33 +1,29 @@
+yumrepo { 'nginx_repo':
+  enabled  => 1,
+  descr    => 'Nginx official repo',
+  baseurl  => 'http://nginx.org/packages/centos/7/$basearch/',
+  gpgcheck => 0,
+}
 
-  yumrepo { 'nginx_repo':
-    enabled  => 1,
-    descr    => 'Nginx official repo',
-    baseurl  => 'http://nginx.org/packages/centos/7/$basearch/',
-    gpgcheck => 0,
-  }
+yumrepo { 'mongodb_repo':
+  enabled  => 1,
+  descr    => 'Mongodb official repo',
+  baseurl  => 'http://repo.mongodb.org/yum/redhat/7/mongodb-org/3.2/x86_64/',
+  gpgcheck => 0,
+}
 
-  yumrepo { 'mongodb_repo':
-    enabled  => 1,
-    descr    => 'Mongodb official repo',
-    baseurl  => 'http://repo.mongodb.org/yum/redhat/7/mongodb-org/3.2/x86_64/',
-    gpgcheck => 0,
-  }
+$packages = [
+  'git',
+  'libxml2',
+  'libxml2-devel',
+  'libxslt-devel',
+]
 
-
- $packages = [
-             	"git",
-	     	"libxml2",
-	     	"libxml2-devel",
-		"libxslt-devel",
-	    ]
- 
 package  { $packages:
-    ensure   => installed,
+  ensure => installed,
 }
 
 package { 'nginx':
-     ensure  => "installed",
-     require => Yumrepo['nginx_repo']
+  ensure  => 'installed',
+  require => Yumrepo['nginx_repo']
 }
-
-
--- manifests/mongodb.pp
+++ manifests/mongodb.pp
@@ -1,21 +1,18 @@
-#include '::mongodb::server'
-
-
 class { '::mongodb::globals':
-	manage_package_repo => 'true',
+  manage_package_repo => true,
 }
 
 class {'::mongodb::server':
-	port => 27019,
-	verbose => true
-	}
+  port    => 27019,
+  verbose => true,
+}
 
 class {'::mongodb::client':}
 
-mongodb_user { mongoadmin:
-  username      => 'mongodb',
+mongodb_user { 'mongoadmin':
   ensure        => present,
-  password_hash =>  mongodb_password('mongodb', 'Q1w2e3r4t5y6'),
+  username      => 'mongodb',
+  password_hash => mongodb_password('mongodb', 'Q1w2e3r4t5y6'),
   database      => 'admin',
   roles         => [ 'readWrite', 'userAdmin', 'dbOwner', 'dbAdmin','userAdminAnyDatabase', 'dbAdmin'],
   tries         => 10,
